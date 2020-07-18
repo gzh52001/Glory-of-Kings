7112546761035b6cd6b26d08bb97faf350aa5785
System Administrator <root@MacBookAir.local>
2020-07-18 14:48:48 +0800

    zujian

---
+++ kings/src/pages/Footer/index.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+import { TabBar } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
+
+class Footer extends Component {
+    constructor() {
+        super();
+        this.state = {
+          selectedTab: '/Home',
+          hidden: false,
+          fullScreen: false,
+        };
+      }
+      
+      componentDidMount() {
+        console.log(this.props)
+        const {location:{pathname}} = this.props;
+         this.setState({
+          selectedTab:pathname
+         })
+      }
+
+    render() {
+        return (
+                <div 
+                style={this.state.fullScreen = { position: 'fixed', width: '100%', bottom: 0 }}>
+                <TabBar
+                unselectedTintColor="#949494"
+                tintColor="#333"
+                barTintColor="white"
+                // hidden={this.state.hidden}
+                >
+                <TabBar.Item
+                    title="首页"
+                    key="/Home"
+                    icon={
+                    <div 
+                    style={{
+                    width: '22px',
+                    height: '22px',
+                    fontSize:'22px'
+                    }}
+                    className={'iconfont icon-icon4'}
+                    />
+                    }
+                    selected={this.state.selectedTab === '/Home'}
+                    selectedIcon={
+                    <div 
+                    style={{
+                        width: '22px',
+                        height: '22px',
+                        fontSize:'22px'
+                    }}
+                    className={'iconfont icon-icon4'}
+                    />
+                    }
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: '/Home',
+                    });
+                    this.props.history.push('/Home')
+                    }}
+                    >
+                </TabBar.Item>
+                <TabBar.Item
+                    icon={
+                    <div style={{
+                        width: '22px',
+                        height: '22px',
+                        fontSize:'24px'
+                        }}
+                        className={'iconfont icon-fenlei'}
+                    />
+                    }   
+                    selected={this.state.selectedTab === '/Fenlei'}
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: '/Fenlei',
+                    });
+                    this.props.history.push('/Fenlei')
+                    }}   
+                    selectedIcon={
+                    <div style={{
+                        width: '22px',
+                        height: '22px',
+                        fontSize:'24px'
+                        }}
+                        className={'iconfont icon-fenlei'}
+                    />
+                    }                   
+                    title="分类"
+                    key="/Fenlei">
+                </TabBar.Item>
+                <TabBar.Item
+                    icon={
+                    <div style={{
+                        width: '22px',
+                        height: '22px',
+                        fontSize:'24px'
+                    }}
+                    className={'iconfont icon-gouwuchekong'}
+                    />
+                    } 
+                    selected={this.state.selectedTab === '/Shopcart'} 
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: '/Shopcart',
+                    });
+                    this.props.history.push('/Shopcart')
+                    }} 
+                    selectedIcon={
+                    <div style={{
+                        width: '22px',
+                        height: '22px',
+                        fontSize:'24px'
+                    }}
+                    className={'iconfont icon-gouwuchekong'}
+                    />
+                    }   
+                    title="购物车"
+                    key="/Shopcart">
+                </TabBar.Item>
+                <TabBar.Item
+                    icon={
+                    <div style={{
+                        width: '22px',
+                        height: '22px',
+                        fontSize:'24px'
+                    }}
+                    className={'iconfont icon-robot'}
+                    />
+                    }  
+                    selected={this.state.selectedTab === '/Mine'} 
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: '/Mine',
+                    });
+                    this.props.history.push('/Mine')
+                    }}  
+                    selectedIcon={
+                    <div style={{
+                        width: '22px',
+                        height: '22px',
+                        fontSize:'24px'
+                    }}
+                    className={'iconfont icon-robot'}
+                    />
+                    }  
+                    title="我"
+                    key="/Mine">
+                </TabBar.Item>
+                </TabBar>
+                </div>
+        )
+    }
+}
+
+Footer = withRouter(Footer);
+export default Footer;
+
+
+
+
+
+
+
+
+
--- kings/src/pages/Mine/index.js
+++ kings/src/pages/Mine/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Icon, Grid } from 'antd-mobile';
 
+import './index.css';
 class Mine extends Component {
     render() {
         return (
-            <div>
-                我的
-            </div>
+            <>              
+                {
+                    <div className='header'>
+                        <a style={{display: 'block'}}>
+                             <img src='https://shp.qlogo.cn/daoju/9999/9999_1/0'/>
+                            <h4>游客</h4>
+                        </a>
+                       
+                    </div>
+                }
+            </>
         )
     }
 }
