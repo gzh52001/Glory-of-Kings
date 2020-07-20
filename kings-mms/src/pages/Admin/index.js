b3e6f8340afec8fffc2541316ba98eabc996cf26
koukous1 <1806867874@qq.com>
2020-07-20 10:23:57 +0800

    管理员模块布局完善

--- kings-mms/src/pages/Admin/index.js
+++ kings-mms/src/pages/Admin/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Table,Button } from 'antd';
+import { Table,Button,Space,Form, Input, Radio } from 'antd';
 import http,{request} from '../../utils/http';
+import './Admin.scss'
 //import reqwest from 'reqwest'
 
 class Admin extends Component {
@@ -50,11 +51,19 @@ class Admin extends Component {
                 },
                 {
                     title: '操作',
-                    dataIndex: <Button>2</Button>,
+                    dataIndex: '',
+                    key: 'x',
+                    render: () => <Space size="middle">
+                    <a>编辑</a>
+                    <a>删除</a>
+                  </Space>,
                     width: 120,
                 }
+                
             ],
-            data: []
+            data: [],
+            username:'',//账户受控
+            name:''//姓名受控
         }
         async componentDidMount(){
             const {data} = await http.get('/admin/adminlist')
@@ -69,26 +78,62 @@ class Admin extends Component {
     //ajax请求数据渲染列表
     //
     
+    //账户input受控
+    changeUserName = (e) =>{
+        this.setState({
+            username:e.target.value
+        })
+    }
+    //姓名input受控
+    changeName = (e) =>{
+        this.setState({
+            name:e.target.value
+        })
+    }
+    //重置按钮功能
+    onReset = () => {
+        this.setState({
+            username:'',
+            name:''
+        })
+      }
     render() {
-        const { columns } = this.state
+        const { columns,username,name } = this.state
         const { data } = this.state
+        const { Column, ColumnGroup } = Table
         return (
-            <Table
-                columns={columns}
-                dataSource={data}
-                pagination={{
-                    pageSize: 20,
-                    position: ['bottomCenter'],
-                    style: {
-                        marginBottom: 0,
+            <div>
+                {/* 搜索表单 */}
+                <Form layout='inline' className='search'>
+                    <Form.Item label="账号">
+                        <Input ref={(ele) => { this.username = ele}} value={username} onChange={this.changeUserName} placeholder="请输入所需查询的账号" />
+                    </Form.Item>
+                    <Form.Item label="姓名">
+                        <Input ref={(ele) => { this.name = ele}} value={name} onChange={this.changeName} placeholder="请输入所需查询的姓名" />
+                    </Form.Item>
+                    <Form.Item className='btn'>
+                        <Button type="primary">查询</Button>
+                        <Button type="primary">新增</Button>
+                        <Button type="primary" onClick={this.onReset}>重置</Button>
+                    </Form.Item>
+                </Form>
+                <Table
+                    columns={columns}
+                    dataSource={data}
+                    pagination={{
+                        pageSize: 20,
+                        position: ['bottomCenter'],
+                        style: {
+                            marginBottom: 0,
 
-                    }
-                }}
-                scroll={{ y: 240 }}
-                bordered={true}
-                scroll={{ y: 'calc(100vh - 37vh)' }}
+                        }
+                    }}
+                    scroll={{ y: 240 }}
+                    bordered={true}
+                    scroll={{ y: 'calc(100vh - 40vh)' }}
 
-            />
+                />
+            </div>
         )
     }
 }
