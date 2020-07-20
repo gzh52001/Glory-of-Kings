80816ea9b1b28e8854a1ea2290ab67d4160ef575
dengleqi <2906801084@qq.com>
2020-07-20 11:27:57 +0800

    添加首页底部导航

--- kings/src/App.js
+++ kings/src/App.js
@@ -9,7 +9,6 @@ import Home from './pages/Home';
 import Fenlei from './pages/Fenlei';
 import Shopcart from './pages/Shopcart';
 import Mine from './pages/Mine';
-import Footer from './pages/Footer';
 
 class App extends React.Component{
   render() {
@@ -22,9 +21,7 @@ class App extends React.Component{
               <Route path={'/Mine'} component={Mine}/>
               <Redirect from={'/'} to={'/Home'} exact/>
           </Switch>
-         <Footer/>
       </div>
-      
     );
   }
 }
--- kings/src/pages/Fenlei/index.js
+++ kings/src/pages/Fenlei/index.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+// 引入样式
+import "./fenlei.css"
+import Footer from '../Footer';
 
-class Fenlei extends Component {
+class List extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
     render() {
         return (
-            <div>
-                分类
+            <div className="list">
+                {/* 头部搜索框 */}
+                <div className="search-bar">
+                    <a className="btn-search" title="搜索"></a>
+                </div>
+
+                {/* 下拉菜单 */}
+                <div className="class-tab">
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+
+                {/* 商品列表 */}
+                <div className="goods-listbox"></div>
+
+                {/* 底部导航路由 */}
+                <Footer/>
             </div>
         )
     }
 }
 
-export default Fenlei;
+export default List;
