0e3bdd9b76170d866895349b3122f5f8dbf8255e
koukous1 <1806867874@qq.com>
2020-07-25 11:38:37 +0800

    商品详情接口

--- kings-server/routers/modules/shopRouter.js
+++ kings-server/routers/modules/shopRouter.js
@@ -89,6 +89,112 @@ router.delete('/del/:id', async (req, res) => {
 });
 
 
+//需求：前台app首页分类数据渲染
+    //精品手办 数码  生活  服饰
+    router.get('/shoplistAll', async (req, res) => {
+        try {
+            //手办
+            const gk = await query("SELECT s1.id,shopType,shopName,nowPrice,shopPic FROM shop s1,shoptype t1 WHERE s1.tid=t1.id AND t1.id=1").catch(err =>{
+                console.log(err);
+                throw Error(err);
+            })
+            //数码
+            const dig = await query("SELECT s1.id,shopType,shopName,nowPrice,shopPic FROM shop s1,shoptype t1 WHERE s1.tid=t1.id AND t1.id=2").catch(err =>{
+                console.log(err);
+                throw Error(err);
+            })
+            //生活
+            const life = await query("SELECT s1.id,shopType,shopName,nowPrice,shopPic FROM shop s1,shoptype t1 WHERE s1.tid=t1.id AND t1.id=3").catch(err =>{
+                console.log(err);
+                throw Error(err);
+            })
+            //服饰
+            const clo = await query("SELECT s1.id,shopType,shopName,nowPrice,shopPic FROM shop s1,shoptype t1 WHERE s1.tid=t1.id AND t1.id=4").catch(err =>{
+                console.log(err);
+                throw Error(err);
+            })
+            let inf = {};
+            if (gk.length && dig.length && life.length && clo.length) {
+                inf = {
+                    code: 2000,
+                    flag: true,
+                    message: '查询成功',
+                    data: {
+                        gk,
+                        dig,
+                        life,
+                        clo
+                    }
+                }
+            } else {
+                inf = {
+                    code: 3000,
+                    flag: false,
+                    message: '查询失败'
+                }
+            }
+    
+            res.send(inf);
+        } catch (err) {
+            console.log(77889);
+            let inf = {
+                code: err.errno,
+                flag: false,
+                message: '查询失败'
+            }
+            res.send(inf);
+        }
+    });
+
+//需求：查询id为xx的商品信息
+    //    /shop/shopSingleList
+    router.get('/shopSingleList/:id', async (req, res) => {
+        let id = req.params.id;
+        try {
+            let sql = `SELECT * FROM shop WHERE id=${id}`;
+            let data = await query(sql);//[{},{}]
+            let inf = {}
+            if (data.length) {
+                //查到数据：查询成功
+                inf = {
+                    code: 2000,
+                    flag: true,
+                    message: '查询成功',
+                    data:data
+                    
+                }
+            } else {
+                //查不到数据:不能登录
+                inf = {
+                    code: 3000,
+                    flag: false,
+                    message: '查询成功'
+                }
+            }
+            res.send(inf);
+        } catch (err) {
+            let inf = {
+                code: err.errno,
+                flag: false,
+                message: '查询失败'
+            }
+            res.send(inf);
+        }
+    });
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
